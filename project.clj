3ad0439023614340fbadb489e5e72e2eb5ea1041
Ilya Savitsky <ipsavitsky234@gmail.com>
2023-09-19 16:19:59 +0100

    Switched from lein to clojure deps dependency management

--- project.clj
+++
@@ -1,9 +0,0 @@
-(defproject clojuregol "0.1.0"
-  :description "Game of Life in Clojure and Raylib"
-  :url "https://github.com/games-of-life/clojuregol"
-  :dependencies [[org.clojure/clojure "1.11.1"]
-                 [clj-raylib "0.0.1"]]
-  :main ^:skip-aot clojuregol.core
-  :target-path "target/%s"
-  :profiles {:uberjar {:aot :all
-                       :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}})
